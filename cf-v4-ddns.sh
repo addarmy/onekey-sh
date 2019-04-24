d8d7da8d01f59c6de8bbf684962b7e897b5700ce
Leeze <44236036+addarmy@users.noreply.github.com>
2019-04-25 01:01:14 +0800

    Update cf-v4-ddns.sh

--- cf-v4-ddns.sh
+++ cf-v4-ddns.sh
@@ -45,7 +45,7 @@ CFTTL=120
 FORCE=false
 
 # Site to retrieve WAN ip, other examples are: bot.whatismyipaddress.com, https://api.ipify.org/ ...
-WANIPSITE="icanhazip.com"
+WANIPSITE="ipv4.ip.sb"
 
 # get parameter
 while getopts k:u:h:z:f: opts; do
